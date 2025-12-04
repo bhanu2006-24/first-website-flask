a34d899bdb3c984561fcad7f884f5984a371f701
bhanu2006-24 <bhanupsaini2024@gmail.com>
2025-12-05 00:15:56 +0530

    made for deploy

--- First App/myapp/config.py
+++ First App/myapp/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'default_secret'
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret'
+    DEBUG = os.environ.get('DEBUG') == 'True'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
